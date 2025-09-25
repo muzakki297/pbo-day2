b825047b9b617f42794de8c1def449febd1a0582
Jibran <branferdian@gmail.com>
2025-09-25 16:56:30 +0700

    initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Iphone iphone = new Iphone();
+
+        iphone.Color = "Gold";
+
+        iphone.nyala();
+        iphone.mati();
+        iphone.berdering();
+    }
+}
---
+++ src/Iphone.java
@@ -0,0 +1,17 @@
+public class Iphone {
+    String Color;
+    String Storage;
+    
+    public void nyala(){
+        System.out.println("Iphone berwarna: " + this.Color + "menyala");
+        
+    }
+    public void mati(){
+        System.out.println("Iphone berwarna: " + this.Color + "mematikan");
+        
+    }
+    public void berdering(){
+        System.out.println("Iphone berwarna: " + this.Color + "berdering");
+        
+    }
+}
